1edcbdf29dafca303f75c0f77f1fcacbdc82f36e
prashantdeshmane <prashantdeshmane2002@gmail.com>
2024-03-15 18:02:44 +0530

    check_from

--- js/registration.js
+++ js/registration.js
@@ -1,44 +1,27 @@
-var currentStep = 1;
-var updateProgressBar;
+  // JavaScript for handling form navigation and validation
+  function nextStep(step) {
+    var currentStep = document.getElementById('step' + step);
+    var inputs = currentStep.querySelectorAll('input[required], select[required]');
+    var isValid = true;
 
-function displayStep(stepNumber) {
-  if (stepNumber >= 1 && stepNumber <= 3) {
-    $(".step-" + currentStep).hide();
-    $(".step-" + stepNumber).show();
-    currentStep = stepNumber;
-    updateProgressBar();
-  }
+    for (var i = 0; i < inputs.length; i++) {
+        if (!inputs[i].value) {
+            isValid = false;
+            inputs[i].classList.add('error');
+        } else {
+            inputs[i].classList.remove('error');
+        }
+    }
+
+    if (isValid) {
+        document.getElementById('step' + step).classList.remove('active');
+        document.getElementById('step' + (step + 1)).classList.add('active');
+    } else {
+        alert('Please fill in all required fields.');
+    }
 }
 
-  $(document).ready(function() {
-    $('#multi-step-form').find('.step').slice(1).hide();
-  
-    $(".next-step").click(function() {
-      if (currentStep < 3) {
-        $(".step-" + currentStep).addClass("animate__animated animate__fadeOutLeft");
-        currentStep++;
-        setTimeout(function() {
-          $(".step").removeClass("animate__animated animate__fadeOutLeft").hide();
-          $(".step-" + currentStep).show().addClass("animate__animated animate__fadeInRight");
-          updateProgressBar();
-        }, 500);
-      }
-    });
-
-    $(".prev-step").click(function() {
-      if (currentStep > 1) {
-        $(".step-" + currentStep).addClass("animate__animated animate__fadeOutRight");
-        currentStep--;
-        setTimeout(function() {
-          $(".step").removeClass("animate__animated animate__fadeOutRight").hide();
-          $(".step-" + currentStep).show().addClass("animate__animated animate__fadeInLeft");
-          updateProgressBar();
-        }, 500);
-      }
-    });
-
-    updateProgressBar = function() {
-      var progressPercentage = ((currentStep - 1) / 2) * 100;
-      $(".progress-bar").css("width", progressPercentage + "%");
-    }
-  });
+function prevStep(step) {
+    document.getElementById('step' + step).classList.remove('active');
+    document.getElementById('step' + (step - 1)).classList.add('active');
+}
